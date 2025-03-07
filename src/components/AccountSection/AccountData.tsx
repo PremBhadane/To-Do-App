db25f1229d7f6c523cf89f1a2cba5f1bc49011d4
PremBhadane <prembhadane69@gmail.com>
2025-03-07 22:10:24 +0530

    4.0 commit

--- src/components/AccountSection/AccountData.tsx
+++ src/components/AccountSection/AccountData.tsx
@@ -32,12 +32,7 @@ const AccountData: React.FC = () => {
 
         <TasksDone />
         <DeleteTasks />
-        <a
-          href="https://github.com/aridsm"
-          className="mt-4 bg-rose-100 p-2 rounded-md text-rose-600 text-center transition hover:bg-rose-200 dark:bg-slate-700/[.3] dark:text-slate-200"
-        >
-          
-        </a>
+        
       </section>
     </LayoutMenus>
   );
